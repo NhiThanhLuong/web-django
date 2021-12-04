99bbccb954e7a6b58261641b8caf4905f794f982
NhiThanhLuong <luongthanhnhi00@gmail.com>
2021-12-04 12:34:23 +0700

    setting model

--- home/views.py
+++ home/views.py
@@ -1,9 +1,45 @@
-from django.shortcuts import render
-from django.http import HttpResponse
+from django import template
+from django.shortcuts import render, get_object_or_404
+from django.http import HttpResponse, Http404, HttpResponseRedirect
+from django.template import loader
+from django.views import generic
+from django.urls import reverse
 
-# Create your views here.
-def index(request):
-    response = HttpResponse()
-    response.writelines('<h1>Hello</h1>')
-    response.write('This is app home')
-    return response
+from .models import Question, Choice
+from home import models
+
+class IndexView(generic.ListView):
+    template_name = 'home/index.html'
+    context_object_name = 'lastest_question_list'
+
+    def get_queryset(self):
+        return Question.objects.order_by('-pub_date')[:5]
+
+class DetailView(generic.DetailView):
+    model = Question
+    template_name = 'home/detail.html'
+
+class ResultsView(generic.DetailView):
+    model = Question
+    template_name = 'home/results.html'
+ 
+def vote(request, question_id):
+    question = get_object_or_404(Question, pk = question_id)
+    try:
+        selected_choice = question.choice_set_get(pk=request.POST['choice'])
+    except (KeyError, Choice.DoesNotExist):
+        return render(request, 'home/detail.html', {
+            'question': question,
+            'error_message': "You didn't select a choice.",
+        })
+    else:
+        selected_choice.votes += 1
+        selected_choice.save()
+    return HttpResponseRedirect(reverse('home:results', args=(question.id,)))
+
+# def detail(request, question_id):
+#     try:
+#         question = Question.objects.get(pk=question_id)
+#     except Question.DoesNotExist:
+#         raise Http404("Question does not exist")
+#     return render(request, 'home/detail.html', {'question': question})
